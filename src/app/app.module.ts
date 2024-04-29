6a4ab5f1a5b5f206749c3c7cac9ec9259bb688dc
Mutsami <wilmutsami@gmail.com>
2024-04-29 12:24:31 +0300

    GCP Firebase Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+export const environment = {
+    production: false,
+    firebase :{
+        apiKey: "AIzaSyBU-l3NO0skNzgXJFbuCF3VlrK6MNqQRGU",
+
+        authDomain: "si-technical-assessment.firebaseapp.com",
+      
+        projectId: "si-technical-assessment",
+      
+        storageBucket: "si-technical-assessment.appspot.com",
+      
+        messagingSenderId: "208476917137",
+      
+        appId: "1:208476917137:web:020a71a0989d24038c1e30",
+      
+        measurementId: "G-VZM8LGCMBP"
+    },
+
+};
